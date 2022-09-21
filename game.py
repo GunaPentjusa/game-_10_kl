d2412380ab6c1e8eaeb6856b6d886eb344dadfd3
Guna Pentjusa <70011067+GunaPentjusa@users.noreply.github.com>
2022-09-21 13:04:35 +0300

    My first commit

---
+++ game.py
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Profils</h1>
+
+<form style="display: grid; gap: 12px; width: 300px">
+
+    <input type="text" placeholder="Lietotājvārds">
+
+    <input type="password" placeholder="Parole">
+
+    <input type="number" placeholder="Garums" >
+
+    <input type="chechbox">
+
+</form>
+</body>
+</html>
